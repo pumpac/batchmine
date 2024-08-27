b3eb1ec54f4ea9d6614df1aa5ca46a2226071b63
pumpac <z7gagaya@outlook.com>
2024-08-27 14:02:12 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    PRIVATE_KEY: '',
+    MINE_TX_PER_MINUTE: 10,
+    BATCH_MINE_AMOUNT: 100,
+    RPC: 'https://rpc.ankr.com/blast'
+}
---
+++ mine.js
@@ -0,0 +1,29 @@
+const config = require('./config.js');
+const ethers = require('ethers');
+const provider = new ethers.JsonRpcProvider(config.RPC);
+const miner = new ethers.Wallet(config.PRIVATE_KEY, provider);
+const PUMPAC_ADDRESS = '0x284c8e910098B737bfEada09A5fC27dfA8020FC0';
+const PUMPAC_ABI = require('./abi.json')
+const pumpac = new ethers.Contract(PUMPAC_ADDRESS, PUMPAC_ABI, miner);
+
+
+async function batchMine (_nonce) {
+    try {
+        const tx = await pumpac.connect(miner).batchMine('0x',config.BATCH_MINE_AMOUNT, { nonce: _nonce, gasLimit: 25000000 })
+        console.log('Mine transaction sent with the nonce', _nonce)
+        tx.wait().then(result => {
+            console.log('Mine transaction confirmed with the nonce', _nonce)
+        })
+    } catch (e) {
+        console.error('An error occurred while executing the transaction with the nonce', _nonce, e)
+    }
+}
+
+provider.getTransactionCount(miner.address).then(nonce => {
+    batchMine(nonce)
+
+    setInterval(() => {
+        batchMine(++nonce)
+    }, 60000 / config.MINE_TX_PER_MINUTE)
+})
+
